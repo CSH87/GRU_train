4c85058fa2cdf35181485f92f82dcc7526ce15de
CSH87 <ms0204251@gmail.com>
2021-02-21 00:43:32 +0800

    Add files via upload
    acc7660version

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
 import pandas as pd
-dataset = pd.read_csv('TrainDataSetOfAll.csv') 
-test_dataset = pd.read_csv('TestDataSetOfAll.csv')
+dataset = pd.read_csv('TrainDataSetOfAll2.csv') 
+test_dataset = pd.read_csv('TestDataSetOfAll2.csv')
 normalize_Layer = tf.keras.layers.LayerNormalization(axis=1)
-test_data_X = np.array(test_dataset.iloc[:, 5:9].values)
-test_data_X = normalize_Layer(test_data_X).numpy()
-#test_data_X = test_data_X / np.linalg.norm(test_data_X)
+test_data_X = np.array(test_dataset.iloc[:, [5,6,7,8]].values)
+#test_data_X = normalize_Layer(test_data_X).numpy()
 test_data_X = test_data_X.reshape(-1,10,4)
 i=0
 test_data_Y = []
@@ -17,16 +18,12 @@ for label in test_dataset.iloc[:, -1].values:
         test_data_Y.append(label)
     i=i+1
 test_data_Y=np.array(test_data_Y, dtype=int)
-"""
-for row in csvfile:
-    print(row)
-"""
+
 i=0
 k=0
 train_data_Y=[]
-train_data_X = np.array(dataset.iloc[:, 5:9].values)
-train_data_X = normalize_Layer(train_data_X).numpy()
-#train_data_X = train_data_X / np.linalg.norm(train_data_X)
+train_data_X = np.array(dataset.iloc[:, [5,6,7,8]].values)
+#train_data_X = normalize_Layer(train_data_X).numpy()
 train_data_X = train_data_X.reshape(-1,10,4) 
 for label in dataset.iloc[:, -1].values:
     if i%10 ==0:
@@ -36,21 +33,30 @@ for label in dataset.iloc[:, -1].values:
 train_data_Y=np.array(train_data_Y, dtype=int)
 
 
-
+#"""
 model = keras.Sequential()
-model.add(layers.GRU(128, input_shape = (10,4), return_sequences= True))
-model.add(layers.Dropout(0.2))
-model.add(layers.GRU(64, return_sequences = False))
-model.add(layers.Dropout(0.2))
-model.add(layers.Dense(64))
-model.add(layers.Dropout(0.2))
+model.add(layers.GRU(64, input_shape = (10,4), return_sequences= True))
+#model.add(layers.Dropout(0.2))
+#model.add(layers.Dense(32))
+model.add(layers.Dense(32, activation='sigmoid'))
+model.add(layers.GRU(32, return_sequences = False))
+#model.add(layers.Dropout(0.2))
+#model.add(layers.Dense(64 , activation='sigmoid'))
+#model.add(layers.Dropout(0.2))
 model.add(layers.Dense(units = 1, activation ='sigmoid'))
 model.summary()
-opt = keras.optimizers.Adam(learning_rate=0.002)
-model.compile(loss="binary_crossentropy", optimizer = opt, metrics= ['accuracy'])
+#opt = keras.optimizers.Adam(learning_rate=0.006)
+model.compile(loss="binary_crossentropy", optimizer = "adam", metrics= ['accuracy'])
 
-
-train_history = model.fit(train_data_X, train_data_Y,batch_size= 32, epochs=100, validation_split = 0.2, verbose=1)
-
+myCheckPoint = ModelCheckpoint('my_model_2.h5', 'val_accuracy', save_best_only= True, verbose=1)
+train_history = model.fit(train_data_X, train_data_Y, batch_size= 32, validation_split = 0.1, epochs=100, verbose=1, callbacks=[myCheckPoint])
+#"""
+#model = keras.models.load_model('acc7537.h5')
+model2 = load_model('my_model_2.h5')
+print('model 1 : \n')
 model.evaluate(test_data_X, test_data_Y)
+print('model 2 : \n')
+model2.evaluate(test_data_X, test_data_Y)
+model.save('my_model_1.h5')
+print('model 1 saved')
 print('end')
